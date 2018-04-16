bb1c96cc67867a14e8483fb466f52ea04be0f51a
Taylor <jtsoulis7@gmail.com>
2018-04-16 16:12:28 +0900

    Update DAGExtend.java
    function that now takes a topology and produces packed structure of nodes so that as many nodes that can run together in parallel are stacked together

--- java/src/DAG/DAGExtend.java
+++ java/src/DAG/DAGExtend.java
@@ -15,59 +15,151 @@ public class DAGExtend{
 	public static int group_mode = 2;
 	public static boolean ng_print = false;
 	public static int NUM_CORES = 1;
-	
+
 
 	public static void main(String args[]) {
-		
-		
+
+
 		/*stack of Node(s) to hold fork/join ranking info*/
 		Stack<Node> jfNodes = new Stack<Node>();
-		
-		
-		
-		Node topology = Dag.random_generate2(num_node, num_edge); 
-		DAGTest t = new DAGTest(num_node); 
-		
+
+		ArrayList<Integer> test = new ArrayList<Integer>();
+
+
+
+		Node topology = Dag.random_generate2(num_node, num_edge);
+		DAGTest t = new DAGTest(num_node);
+
 		drawDag(topology);
-		
-		
+		resetDag(topology);
+
+		ArrayList<ArrayList<Integer>> blocks = new ArrayList<ArrayList<Integer>>();
+
+		arrangeByPrecCons(topology, blocks);
+
+		System.out.println();
+
+		printBlocks(blocks);
+
+
+
+
 	}
-	
-	/*This code takes the first node in graph and adds it to a queue.
-	 * While the queue is not empty, it will look at each node in the queue
-	 * and determine if that node has multiple previous or multiple next nodes,
-	 * if it does, it will then add that node to a stack. It will then get the
-	 * list of next nodes from the current node that was in the queue and 
-	 * add all of those nodes to the queue to be explored. By the end of the
-	 * while loop, all nodes should have been examined and all fork and join
-	 * nodes will have been added to stack which is returned to calling function.
-	 * I employed a BFS algorithm for looking at this graph. Refer to wikipedia if confused*/
-	public static Stack<Node> findJoinForks(Node node)
+
+
+	/*
+	 * results in a transformation of input DAG topology. 'blocks' is a list of lists, each inner list containing the nodes that can be executed
+	 * given that its predecessors have already been executed.
+	 *
+	 * */
+	public static void arrangeByPrecCons(Node node, ArrayList<ArrayList<Integer>> blocks)
 	{
-		Node cur = node;
-		cur.visit = true;
-		Stack<Node> s = new Stack<Node>();
-		Queue<Node> q = new LinkedList<Node>();
-		q.add(cur);
-		while(!q.isEmpty()) {
-			Node t = q.remove();
-			ArrayList<Node> tnext = t.getNext();
-			if(tnext.size() > 1 || t.getPrev().size() > 1) {
-				s.push(t);
+
+		ArrayList<Integer> block = new ArrayList<Integer>();
+		block = getBlock(node);
+		while(!block.isEmpty())
+		{
+			blocks.add(block);
+			block = getBlock(node);
+		}
+
+		resetDagAccounted(node);
+
+	}
+
+	public static boolean arePredecessorsAccounted(Node node)
+	{
+		ArrayList<Node> prevs = node.getPrev();
+		int count = 0;
+		for(int i = 0; i < prevs.size(); i++)
+		{
+			if(prevs.get(i).accounted == true) {
+				count++;
 			}
-			for(int i = 0; i < tnext.size(); i++) {
-				Node o = tnext.get(i);
-				if(o.visit != true) {
-					o.visit = true;
-					q.add(o);
+		}
+
+		if (count == prevs.size()) {
+			return true;
+		}
+		else return false;
+	}
+
+	public static ArrayList<Integer> getBlock(Node node)
+	{
+		resetDag(node);
+		ArrayList<Integer> block = new ArrayList<Integer>();
+
+		node.visit = true;
+		Queue<Node> q = new LinkedList<Node>();
+		Queue<Node> accountedFor = new LinkedList<Node>();
+		q.add(node);
+		while(!q.isEmpty()) {
+			Node target = q.remove();
+			if(target.accounted == false && arePredecessorsAccounted(target)) {
+				//target.accounted = true;
+				block.add(target.getId());
+				accountedFor.add(target);
+			}
+			ArrayList<Node> nexts = target.getNext();
+			for(int i = 0; i < nexts.size(); i++) {
+				Node next = nexts.get(i);
+				if(next.visit != true) {
+					next.visit = true;
+					q.add(next);
 				}
 			}
 		}
-		
-		return s;
-		
+
+
+		while(!accountedFor.isEmpty())
+		{
+			Node acct = accountedFor.remove();
+			//System.out.print(acct.getId() + " ");
+			acct.accounted = true;
+		}
+		//System.out.println();
+
+		resetDag(node);
+
+		return block;
 	}
-	
+
+
+	//verified this works as expected with testcases
+	public static void printBlocks(ArrayList<ArrayList<Integer>> blocks)
+	{
+		for (int i = 0; i < blocks.size(); i++)
+		{
+			for (int j = 0; j < blocks.get(i).size(); j++)
+			{
+				System.out.print(blocks.get(i).get(j) + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void resetDag(Node node)
+	{
+		ArrayList<Node> nexts = node.getNext();
+
+		if(node.visit == true)
+			node.visit = false;
+
+		for(int i = 0; i < nexts.size(); ++i)
+			resetDag(nexts.get(i));
+	}
+
+	public static void resetDagAccounted(Node node)
+	{
+		ArrayList<Node> nexts = node.getNext();
+
+		if(node.accounted == true)
+			node.accounted = false;
+
+		for(int i = 0; i < nexts.size(); ++i)
+			resetDag(nexts.get(i));
+	}
+
 	public static void drawDag(Node node)
 	{
 		ArrayList<Node> nexts = node.getNext();
@@ -89,11 +181,11 @@ public class DAGExtend{
 		for(int i = 0; i < nexts.size(); ++i)
 			drawDag(nexts.get(i));
 	}
-	
+
 	public static ArrayList<ArrayList<Integer>> trans_dag (NodeGroup dag)
 	{
 		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
-		
+
 		NodeGroup cur = dag;
 		while (cur != null)
 		{
@@ -109,4 +201,41 @@ public class DAGExtend{
 
 		return temp;
 	}
+
+	/*This code takes the first node in graph and adds it to a queue.
+	 * While the queue is not empty, it will look at each node in the queue
+	 * and determine if that node has multiple previous or multiple next nodes,
+	 * if it does, it will then add that node to a stack. It will then get the
+	 * list of next nodes from the current node that was in the queue and
+	 * add all of those nodes to the queue to be explored. By the end of the
+	 * while loop, all nodes should have been examined and all fork and join
+	 * nodes will have been added to stack which is returned to calling function.
+	 * I employed a BFS algorithm for looking at this graph. Refer to wikipedia if confused*/
+	/*
+	public static Stack<Node> findJoinForks(Node node)
+	{
+		Node cur = node;
+		cur.visit = true;
+		Stack<Node> s = new Stack<Node>();
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(cur);
+		while(!q.isEmpty()) {
+			Node t = q.remove();
+			ArrayList<Node> tnext = t.getNext();
+			if(tnext.size() > 1 || t.getPrev().size() > 1) {
+				s.push(t);
+			}
+			for(int i = 0; i < tnext.size(); i++) {
+				Node o = tnext.get(i);
+				if(o.visit != true) {
+					o.visit = true;
+					q.add(o);
+				}
+			}
+		}
+
+		return s;
+
+	}
+	*/
 }
